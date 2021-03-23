1d28f9a01089d8eaceb3e64b4780f7f793f417b1
강민창 <new0822@naver.com>
2021-03-23 20:17:19 +0900

    프로젝트 설정하기

---
+++ hello-world/1장/babel/temp/jsx.js
@@ -0,0 +1,7 @@
+<div className="box">  //자바스크립트에선 class가 예약어
+    <Title text="hello world" width={200} />
+    <button onClick={() => { }}>좋아요</button>
+    <a href="/home" style={{ marginTop: '10px', color: 'red' }}> //자바스크립트에선 하이픈이 마이너스로 인식
+        홈으로 이동
+    </a>
+</div>
